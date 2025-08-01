71ba91334636eca523f3872d1f4a4400aff906f7
rafal3008 <rafal.pyzalski08@gmail.com>
2025-08-01 22:28:17 +0200

    Added text client

--- src/core/main.py
+++ src/core/main.py
@@ -1,10 +1,36 @@
 from core.manager import BudgetManager
 from core.budget import BudgetEntry
+from dateutil.parser import parse
 
 
 FILE_PATH = "../data/budget.json"
 
 manager = BudgetManager(FILE_PATH)
-new_entry = BudgetEntry(100, "Pranie", "25.06.2025", "Test3")
-manager.add_entry(new_entry)
+
+while True:
+    print("What would you like to do? 1 - add entry, 2 - show entries, 3 - exit")
+    answer = input()
+    match answer:
+        case "1":
+            print("Specify amount: ")
+            amount = float(input())
+            print("Expense category: ")
+            category = input()
+            print("Date of expense: ")
+            date = parse(input()).strftime("%d.%m.%Y")
+            print("Add note to expense: ")
+            note = input()
+            new_entry = BudgetEntry(amount, category, date, note)
+            manager.add_entry(new_entry)
+        case "2":
+            print("Specify type of entry(income/expenses): ")
+            entry_type = input().strip().lower()
+            print(f"Current entries: {manager.get_entries()}\n")
+            entries = manager.get_entries(entry_type)
+            for e in entries:
+                print(f"{e['timestamp']} - {e['category']} - {e['amount']} ({e['note']})")
+
+        case "3":
+            break
+
 
