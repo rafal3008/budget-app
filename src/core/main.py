833344bd1193bdcba22d177a71b97f740a6cab8e
rafal3008 <rafal.pyzalski08@gmail.com>
2025-08-02 01:18:08 +0200

    Added filtering and partial exceptions. Improved text cli

--- src/core/main.py
+++ src/core/main.py
@@ -1,36 +1,69 @@
 from core.manager import BudgetManager
 from core.budget import BudgetEntry
 from dateutil.parser import parse
+from datetime import datetime
 
 
 FILE_PATH = "../data/budget.json"
 
 manager = BudgetManager(FILE_PATH)
 
-while True:
-    print("What would you like to do? 1 - add entry, 2 - show entries, 3 - exit")
-    answer = input()
-    match answer:
-        case "1":
-            print("Specify amount: ")
-            amount = float(input())
-            print("Expense category: ")
-            category = input()
-            print("Date of expense: ")
-            date = parse(input()).strftime("%d.%m.%Y")
-            print("Add note to expense: ")
-            note = input()
-            new_entry = BudgetEntry(amount, category, date, note)
-            manager.add_entry(new_entry)
-        case "2":
-            print("Specify type of entry(income/expenses): ")
-            entry_type = input().strip().lower()
-            print(f"Current entries: {manager.get_entries()}\n")
-            entries = manager.get_entries(entry_type)
-            for e in entries:
-                print(f"{e['timestamp']} - {e['category']} - {e['amount']} ({e['note']})")
+def entry_prompt():
+    print("\n===Adding new entry===")
+    amount = float(input("Amount: ")) or 0
+    category = input("Category: ") or "N/A"
+    date_str = input("Date: ") or str(datetime.now())
+    date = parse(date_str).strftime("%d.%m.%Y")
+    note = input("Note [optional]: ")
+    return BudgetEntry(amount, category, date, note)
 
-        case "3":
-            break
+def filter_prompt():
+    print("\n===Filtering entries=== ")
+    entry_type = input("Type of entry(expenses/income): ").strip().lower() or "expenses"
+    category = input("Category [optional]: ").strip() or None
+
+    from_date_str = input("From (dd.mm.yyyy)[optional]: ").strip()
+    from_date = parse(from_date_str) if from_date_str else None
+
+    to_date_str = input("To (dd.mm.yyyy)[optional]: ").strip()
+    to_date = parse(to_date_str) if to_date_str else None
+
+    return entry_type, category, from_date, to_date
+
+def display_entries(entries):
+    print("\n===Found entries===")
+    if not entries:
+        print("No entries found.")
+    for entry in entries:
+        print(f"{entry['timestamp']} | {entry['category']} | {entry['amount']} | {entry['note']} ")
+
+def main_menu():
+
+    while True:
+        print("\n====Budget-app text client - MAIN MENU====")
+        print("1. Add new entry")
+        print("2. Show entries(filtered)")
+        print("3. Exit")
+
+        answer = input("What would you like to do?: ").strip()
+
+        match answer:
+            case "1":
+                entry = entry_prompt()
+                entry_type = input("Type of entry(expenses/income): ").strip().lower() or "expenses"
+                manager.add_entry(entry, entry_type)
+                print("Added new entry")
+            case "2":
+                entry_type, category, from_date, to_date = filter_prompt()
+                entries = manager.get_entries(entry_type, category, from_date, to_date)
+                display_entries(entries)
+            case "3":
+                print("Exiting")
+                break
+            case _:
+                print("Invalid input. Try again")
+
+if __name__ == "__main__":
+    main_menu()
 
 
--- src/core/manager.py
+++ src/core/manager.py
@@ -1,4 +1,5 @@
 from io_handler import json_handler as handler
+from dateutil.parser import parse
 
 class BudgetManager:
     def __init__(self, file_path):
@@ -11,7 +12,25 @@ class BudgetManager:
         self.data[entry_type].append(entry.to_dict())
         handler.save_data(self.file_path, self.data)
 
-    def get_entries(self, entry_type="expenses"):
+    def get_entries(self, entry_type="expenses", category=None, from_date=None, to_date=None):
         if entry_type not in self.data:
             raise ValueError(f"{entry_type} is not a valid entry type")
-        return self.data[entry_type]
+
+        filtered_entries = []
+
+        for entry in self.data[entry_type]:
+            try:
+                entry_date = parse(entry["timestamp"])
+            except Exception:
+                continue
+
+            if category is not None and entry['category'] != category:
+                continue
+            if from_date is not None and entry_date < from_date:
+                continue
+            if to_date is not None and entry_date > to_date:
+                continue
+
+            filtered_entries.append(entry)
+
+        return filtered_entries
